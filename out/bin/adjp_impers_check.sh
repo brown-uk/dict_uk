2ecc6d3e6f41059af127c47cf89199c20b92b524
Andriy Rysin <arysin@gmail.com>
2015-12-13 17:41:31 -0500

    small fixes

--- out/bin/adjp_impers_check.sh
+++ out/bin/adjp_impers_check.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-groovy bin/adjp_impers_check.groovy < dict_corp_lt.txt > mismatch.txt
+groovy -cp ../scripts/common ../scripts/tools/adjp_impers_check.groovy < dict_corp_lt.txt > mismatch.txt
