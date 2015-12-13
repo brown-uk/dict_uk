d9aad418a3f5683ff142dc730dcb8750dffa2f92
Andriy Rysin <arysin@gmail.com>
2015-12-13 12:21:26 -0500

    verb_reverse in groovy

--- scripts/expand/verb_reverse.groovy
+++ scripts/expand/verb_reverse.groovy
@@ -1,124 +1,150 @@
 #!/usr/bin/env groovy
 
+import java.util.regex.*;
+import groovy.util.CliBuilder;
+import groovy.transform.CompileStatic;
 
-import re
-import sys
-
+//
+// Generates affix file for reverse verb forms 
+//
+//@CompileStatic
 class VerbReverse {
-	//
-    // Generates affix file for reverse verb forms 
-	//
+    final Pattern RE_GENERIC = Pattern.compile(/.*([аяийіїоуюв]|те)$/)
+    final Pattern RE_ADVP = Pattern.compile(/.*[чш]и$/)
 
-    public VerbReverse():
-        self.RE_GENERIC = Pattern.compile(".*([аяийіїоуюв]|те)$")
-        self.RE_ADVP = Pattern.compile(".*[чш]и$")
+	class Re {
+    	def match(regex, str) {
+			return Pattern.matches(regex, str)
+		}
+		def sub(regex, repl, str) {
+			return str.replaceAll(regex, repl)
+		}
+	}
+	
+	final Re re = new Re()
+	
+	def len(obj) {
+		return obj.size()
+	}
+	
         
-    def generate_rev(self, line):
-        line = line.strip()
+    def generate_rev(String line) {
+        line = line.trim()
 
-        line_parts = line.split("#")
-        left = line_parts[0]
+        def line_parts = line.split("#")
+		
+		if( line_parts.size() == 0 ) {
+			line_parts = [""]
+		}
+		
+        String left = line_parts[0]
         
+		String comment
         if( len(line_parts) > 1 )
             comment = line_parts[1]
-        else:
+        else
             comment = ""
 
         if( left.contains("group v") ) {
             line = line.replace("group v", "group vr")
-            if left.startsWith("group v"):
+            if( left.startsWith("group v") ) {
                 line += "\n\n# Зворотня форма дієслів (-ся та -сь)\n"
                 line += "ся\tсь	тися		#  ~тися  ~тись    @ verb:rev:inf\n\n"
-
+            }
             return line
         }
 
-        if( left.strip().endswith(":") )
+        if( left.trim().endsWith(":") )
             return line.replace("ти:", "тися:")
           
-        columns = left.split()
+        def columns = left.split()
           
         if( len(columns) < 2 )
             return line
           
-        dual = false
-        if "advp" in comment:
+        def dual = false
+		def suff
+        if( comment.contains("advp") ) 
             suff = "сь"
-        elif re.match(".*те$", columns[1]) and ":s:3" in comment:
+        else if( re.match(/.*те$/, columns[1]) && comment.contains(":s:3") )
             suff = "ться"
-        elif self.RE_GENERIC.match(columns[1]):
-            dual = True
+        else if( RE_GENERIC.matcher(columns[1]).matches() ) {
+            dual = true
             suff = "ся"
-        elif re.match(".*[еє]$", columns[1]):
+        }
+        else if( re.match(/.*[еє]$/, columns[1]) )
             suff = "ться"
-        else:
+        else
             suff = "ся"
           
-        columns = self.convert_left(columns, suff)
+        columns = convert_left(columns, suff)
         
-        comment = self.convert_comment(comment, suff)
+        comment = convert_comment(comment, suff)
         
 
-        out_line = "\t".join(columns) + "\t\t#\t\t" + comment
+        def out_line = columns.join("\t") + "\t\t#\t\t" + comment
         
-        if dual:
-            columns[1] = re.sub("ся$", "сь", columns[1])
+        if( dual ) {
+            columns[1] = re.sub(/ся$/, "сь", columns[1])
 
             out_line += "\n"
             comment = comment.replace("ся\t", "сь\t")
-            out_line += "\t".join(columns) + "\t\t#\t\t" + comment
-        
+            out_line += columns.join("\t") + "\t\t#\t\t" + comment
+        }
         return out_line
+    }
         
-        
-    def convert_comment(self, comment, suff):
+    def convert_comment(comment, suff) {
         comment = comment.replace("verb:", "verb:rev:")
         comment = comment.replace("advp:", "advp:rev:")
         
-        columns = comment.split("@")
-        left_cols = columns[0].split()
+        def columns = comment.split("@")
+        def left_cols = columns[0].split()
         
         left_cols[0] += suff
         left_cols[1] += suff
         
-        comment = "\t".join(left_cols) + "\t\t@" + columns[1]
+        comment = left_cols.join("\t") + "\t\t@" + columns[1]
         
         return comment
+	}   
         
-        
-    def convert_left(self, columns, suff):
-        if columns[0] == "0":
+    def convert_left(columns, suff) {
+        if( columns[0] == "0" )
             columns[0] = "ся"
-        else:
+        else
             columns[0] += "ся"
 
-        if columns[1] == "0":
+        if( columns[1] == "0" )
             columns[1] = "ся"
-        else:
+        else
             columns[1] += suff
         
-        if len(columns) > 2:
+        if( len(columns) > 2 )
             columns[2] += "ся"
         
         return columns
+    }
 }          
 
 
 //if __name__ == "__main__":
 
-    def cnv = VerbReverse()
+    def cnv = new VerbReverse()
 
     convert = true
     System.in.readLines().each {
         
-        if convert:
+		def line = it
+        if( convert ) {
             line = cnv.generate_rev(line)
             println(line)
-        else:
-            if line.contains("ся "):
+        }
+        else
+            if( line.contains("ся ") )
                 println(line)
 
-        # v5 is special - don't generate reverse affixes for it
-        if line.contains("group "):
+        // v5 is special - don't generate reverse affixes for it
+        if( line.contains("group ") )
             convert = ! line.contains(" v5")
 	}
