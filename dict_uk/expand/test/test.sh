123bdc29744b57f7e2a1c27df624218400f91248
Andriy Rysin <arysin@gmail.com>
2015-12-27 23:27:59 -0500

    cleanup for python tests

--- dict_uk/expand/test/test.sh
+++ dict_uk/expand/test/test.sh
@@ -4,6 +4,8 @@ BASE=../../..
 
 FILE=test.lst
 CORP_LT_FILE=dict_corp_lt.txt
+CORP_VIS_FILE=dict_corp_vis.txt
+RULES_LT_FILE=dict_rules_lt.txt
 
 function diff_u() {
     FIL="$1"
@@ -13,17 +15,17 @@ function diff_u() {
 
 
 rm -f $FILE.tag.diff
-../expand.py -aff $BASE/data/affix -corp -indent -mfl --uncontr -stats -wordlist < $FILE > $FILE.tag &&\
-diff -u prev/$FILE.tag $FILE.tag > $FILE.tag.diff && \
+../expand.py -aff $BASE/data/affix -corp -indent -mfl --uncontr -stats -wordlist < $FILE > $CORP_VIS_FILE &&\
+diff -u prev/$CORP_VIS_FILE $CORP_VIS_FILE > $CORP_VIS_FILE.diff && \
 diff -u prev/$CORP_LT_FILE $CORP_LT_FILE > $CORP_LT_FILE.diff && \
 diff_u lemmas.txt && \
 diff_u words.txt && \
 diff_u tags.txt && \
-echo "No regressions" && rm $FILE.tag $CORP_LT_FILE *.diff
+echo "No regressions" && rm $CORP_VIS_FILE $CORP_LT_FILE *.diff
 
 echo
 echo
 
-../expand.py -aff $BASE/data/affix -mfl < $FILE > ${FILE}_rules.tag && \
-diff -u prev/${FILE}_rules.tag ${FILE}_rules.tag > ${FILE}_rules.tag.diff && \
-echo "No regressions" && rm ${FILE}_rules.tag.diff
+../expand.py -aff $BASE/data/affix -mfl < $FILE > $RULES_LT_FILE && \
+diff -u prev/$RULES_LT_FILE $RULES_LT_FILE > $RULES_LT_FILE.diff && \
+echo "No regressions" && rm $RULES_LT_FILE*
