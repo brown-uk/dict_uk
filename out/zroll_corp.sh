bd1799ef835ad33dbaf59788fcb2b702f54a2bd0
Andriy Rysin <arysin@gmail.com>
2016-02-03 15:21:27 -0500

    build script updates

--- out/zroll_corp.sh
+++ out/zroll_corp.sh
@@ -2,9 +2,11 @@
 
 # Rotates all newely generated files into prev/ directory for the following regression checking
 
-FILES="dict_corp_vis.txt dict_corp_lt.txt"
+FILE1="dict_corp_lt.txt"
+FILE2="dict_corp_vis.txt"
 
 #mv -f *.dups *.uniq prev/
-cp -f $FILES prev/
+cp -f $FILE1 prev/
+mv -f $FILE2 prev/
 mv words.txt lemmas.txt tags.txt words.txt tags.txt prev/
 mv dict_stats.txt stats/
