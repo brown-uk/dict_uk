bc7bb4082652190f7c1bdc3cc37b1ed44ddd64a1
Andriy Rysin <arysin@gmail.com>
2016-02-06 11:20:44 -0500

    doc update; stats update

--- out/zroll_corp.sh
+++ out/zroll_corp.sh
@@ -7,6 +7,6 @@ FILE2="dict_corp_vis.txt"
 
 #mv -f *.dups *.uniq prev/
 cp -f $FILE1 prev/
-mv -f $FILE2 prev/
+cp -f $FILE2 prev/
 mv words.txt lemmas.txt tags.txt words.txt tags.txt prev/
 mv dict_stats.txt stats/
