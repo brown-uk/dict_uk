45ade1d88b281003946fc170dc56d9e483bcc965
Andriy Rysin <arysin@gmail.com>
2017-01-02 13:24:56 -0500

    hide package names in logging output

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -5,7 +5,7 @@ import static ch.qos.logback.classic.Level.INFO
 
 appender("STDOUT", ConsoleAppender) {
   encoder(PatternLayoutEncoder) {
-	pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+	pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{0} - %msg%n"
   }
 }
 root(INFO, ["STDOUT"])
