b3dbf8a06939f8ac3cf53049ed65dc98574efd3b
Andriy Rysin <arysin@gmail.com>
2017-04-12 14:52:36 -0400

    New words
    Fixes #126

--- out/zroll_corp.sh
+++ out/zroll_corp.sh
@@ -6,8 +6,8 @@ FILE1="dict_corp_lt.txt"
 FILE2="dict_corp_vis.txt"
 
 #mv -f *.dups *.uniq prev/
-cp -f $FILE1 prev/
-cp -f $FILE2 prev/
-cp -f words_spell.txt prev/
+rm -f prev/$FILE1 ; cp -f $FILE1 prev/
+rm -f prev/$FILE2 ; cp -f $FILE2 prev/
+rm -f prev/words_spell.txt; cp -f words_spell.txt prev/
 mv words.txt lemmas.txt prev/
 mv dict_stats.txt stats/
