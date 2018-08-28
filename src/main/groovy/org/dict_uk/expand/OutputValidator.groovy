edcd36ca634074513fb7f52df85a9743621858c3
Andriy Rysin <arysin@gmail.com>
2018-08-28 07:34:16 -0400

    few new words

--- src/main/groovy/org/dict_uk/expand/OutputValidator.groovy
+++ src/main/groovy/org/dict_uk/expand/OutputValidator.groovy
@@ -14,7 +14,7 @@ class OutputValidator {
 	static Logger log = LoggerFactory.getLogger(OutputValidator.class);
 	
 	static final Pattern WORD_RE = Pattern.compile("[а-яіїєґА-ЯІЇЄҐ][а-яіїєґА-ЯІЇЄҐ']*(-[а-яіїєґА-ЯІЇЄҐ']*)*|[А-ЯІЇЄҐ][А-ЯІЇЄҐ-]+|[а-яіїєґ]+\\.(-[а-яіїєґ]+\\.)?")
-	static final Pattern POS_RE = Pattern.compile("(noun:([iu]n)?anim:|noun:.*:&pron|verb(:rev)?:(im)?perf:|advp:(im)?perf|adj:[mfnp]:|adv|numr:|prep|part|intj|conj:|onomat|foreign|noninfl).*")
+	static final Pattern POS_RE = Pattern.compile("(noun:([iu]n)?anim:|noun:.*:&pron|verb(:rev)?:(im)?perf:|advp:(im)?perf|adj:[mfnp]:|adv|numr:|prep|part|intj|conj:|onomat|foreign|noninfl|number).*")
     static final List<String> IGNORED_NOUNS = ["бельмеса", "давніх-давен", "основанья", "предку-віку", "роб", "свободівець", "шатер",
             "галай-балай", "вепр", "вихідець", "гратами", "мати-одиночка", "кінця-краю", "усіх-усюд", "Таганріг", "Хмельницьк", "буйвол"]
     static final List<String> IGNORED_VERBS = ["хтітися", "жити-бути", "писано-переписано", "забракнути"]
