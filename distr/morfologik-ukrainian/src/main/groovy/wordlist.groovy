bc17813bf9562ddc0362f2746d138737b4c382be
Andriy Rysin <arysin@gmail.com>
2020-05-27 12:20:51 -0400

    script to generate word frequencies

---
+++ distr/morfologik-ukrainian/src/main/groovy/wordlist.groovy
@@ -0,0 +1,43 @@
+#!/bin/env groovy
+
+def words = [:].withDefault{0}
+
+def allTagged = new File('../../../build/tmp/all.tagged.tmp').readLines().collect{ it.split(/\t/)[1] } as Set
+
+new File('all.tagged.freq.txt').eachLine { String line ->
+    if( ! line.trim() )
+        return
+
+    String[] parts = line.split(/\t/)
+
+    try {
+        String w = parts[1]
+
+    if( ! (w ==~ /^[а-яіїєґА-ЯІЇЄҐ]([а-яіїєґА-ЯІЇЄҐ'-]*[а-яіїєґА-ЯІЇЄҐ])?$/) )
+        return
+
+    int f = parts[0] as int
+
+    if( w in allTagged ) {
+        words[w] += f
+    }
+    w = w.toLowerCase()
+    if( w in allTagged ) {
+        words[w] += f
+    }
+    }
+    catch(e) {
+        println "Failed in $line"
+        throw e
+    }
+}
+
+
+def out = new File('uk_wordlist.xml')
+out.text = '<wordlist locale="uk_UA" description="Ukrainian" date="'+new Date().getTime()+'" version="1">\n'
+
+words.toSorted{ -it.value }.each {
+    out << "<w f=\"${it.value}\">${it.key}</w>\n"
+}
+
+out << '</wordlist>\n'
