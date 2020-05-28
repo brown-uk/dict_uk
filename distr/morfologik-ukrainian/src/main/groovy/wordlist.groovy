53e0c8aebe6864119fbd978e619f0d4b3d8cc4d7
Andriy Rysin <arysin@gmail.com>
2020-05-28 14:00:26 -0400

    update word frequencies

--- distr/morfologik-ukrainian/src/main/groovy/wordlist.groovy
+++ distr/morfologik-ukrainian/src/main/groovy/wordlist.groovy
@@ -1,8 +1,10 @@
 #!/bin/env groovy
 
+def freqMin = 10
 def words = [:].withDefault{0}
 
-def allTagged = new File('../../../build/tmp/all.tagged.tmp').readLines().collect{ it.split(/\t/)[1] } as Set
+def allTagged = new File('../../../build/tmp/all.tagged.tmp').readLines().collect{ it.split(/\t/, 2)[0] } as Set
+println "Unqiue tagged: " + allTagged.size()
 
 new File('all.tagged.freq.txt').eachLine { String line ->
     if( ! line.trim() )
@@ -37,6 +39,9 @@ def out = new File('uk_wordlist.xml')
 out.text = '<wordlist locale="uk_UA" description="Ukrainian" date="'+new Date().getTime()+'" version="1">\n'
 
 words.toSorted{ -it.value }.each {
+    if( it.value < freqMin )
+        return
+
     out << "<w f=\"${it.value}\">${it.key}</w>\n"
 }
 
