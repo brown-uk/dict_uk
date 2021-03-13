cb53aa2d625662b72f19b3370b12018c805b11de
Andriy Rysin <arysin@gmail.com>
2021-03-13 13:55:43 -0500

    new words

--- src/main/groovy/org/dict_uk/expand/OutputValidator.groovy
+++ src/main/groovy/org/dict_uk/expand/OutputValidator.groovy
@@ -13,7 +13,7 @@ import groovy.transform.TypeChecked;
 class OutputValidator {
 	static Logger log = LoggerFactory.getLogger(OutputValidator.class);
 	
-	static final Pattern WORD_RE = Pattern.compile("'?[а-яіїєґА-ЯІЇЄҐ][а-яіїєґА-ЯІЇЄҐ']*(-[а-яіїєґА-ЯІЇЄҐ']*)*|[А-ЯІЇЄҐ][А-ЯІЇЄҐ-]+|[а-яіїєґА-ЯІЇЄҐ][а-яіїєґ]*\\.(-[а-яіїєґ]+\\.)?")
+	static final Pattern WORD_RE = Pattern.compile("'?[а-яіїєґА-ЯІЇЄҐ][а-яіїєґА-ЯІЇЄҐ']*(-[а-яіїєґА-ЯІЇЄҐ']*)*|[А-ЯІЇЄҐ][А-ЯІЇЄҐ-]+|[а-яіїєґА-ЯІЇЄҐ][а-яіїєґ]*\\.(-[а-яіїєґ]+\\.)?|[А-ЯІЇЄҐ]+\\.")
 	static final Pattern POS_RE = Pattern.compile("(noun:([iu]n)?anim:|noun:.*:&pron|verb(:rev)?:(im)?perf:|advp:(im)?perf|adj:[mfnp]:|adv|numr:|prep|part|intj|conj:|onomat|foreign|noninfl|number).*")
 
 	final List<String> ALLOWED_TAGS = getClass().getResource("tagset.txt").readLines()
