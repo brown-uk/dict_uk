e7bde64171b9072d4694bb24ced7d572447258c3
Andriy Rysin <arysin@gmail.com>
2022-08-14 16:05:12 -0400

    fix 3 comment positions

--- src/tools/groovy/dict/AlignColumn.groovy
+++ src/tools/groovy/dict/AlignColumn.groovy
@@ -6,24 +6,31 @@ System.in.eachLine { line ->
         return
     }
 
-
-    if( line.contains("#>") ) {
-        def (left, right) = line.split("#>")
-
-        left = left.replaceFirst(/\s+$/, '').padRight(48)
-
-        println left + "#>" + right
+    String[] parts = line.split(/\s*#\s*/)
+    
+    String newLine = parts[0]
+    String rv = ""
+    String repl  = ""
+    String other = ""
+    
+    parts[1..-1].each { String part ->
+        if( part.startsWith('>') )
+            repl = part
+        else if( part.startsWith('rv_') )
+            rv = part
+        else  // if( part.startsWith('rv_') )
+            other = part
     }
-    else
-    if( line.contains("#") ) {
-        def (left, right) = line.split("#", 2)
 
-        left = left.replaceFirst(/\s+$/, '').padRight(48)
-
-        if( ! left.endsWith(' ') ) {
-            left += '  '
-        }
-
-        println left + "#" + right
+    if( other ) {
+        newLine = newLine.padRight(40) + "  # $other"
     }
+    if( rv ) {
+        newLine = newLine.padRight(60) + "  # $rv"
+    }
+    if( repl ) {
+        newLine = newLine.padRight(76) + "  #$repl"
+    }
+    
+    println newLine
 }
