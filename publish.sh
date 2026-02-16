8202e122cbd7f08d225a736f0f5cafaaee0d5279
Andriy Rysin <arysin@gmail.com>
2026-02-16 11:48:12 -0500

    adjust build scripts

--- publish.sh
+++ publish.sh
@@ -5,13 +5,13 @@ grep SNAPSHOT VERSION && {
     exit 1
 }
 
-./gradlew -p distr/morfologik-ukrainian publish
-# ./gradlew -p distr/morfologik-ukrainian preparePublishZip
+./gradlew -p distr/morfologik-ukrainian publishToMavenLocal
+#./gradlew -p distr/morfologik-ukrainian preparePublishZip
 
 exit 0
 
 version=$(cat VERSION)
 cd "$HOME/.m2/repository"
 # zip -b "$HOME/.m2/repository" "morfologik-ukrainian-lt-${version}.zip" "ua/net/nlp/morfologik-ukrainian-lt/$version/*"
-zip "morfologik-ukrainian-lt-${version}.zip" "ua/net/nlp/morfologik-ukrainian-lt/$version/*"
+zip -r "morfologik-ukrainian-lt-${version}.zip" "ua/net/nlp/morfologik-ukrainian-lt/$version/*"
 cd ~
