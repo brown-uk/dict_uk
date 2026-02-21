ca1e94c3a718566a37e6cfb0e8ba8982aac9e83e
Andriy Rysin <arysin@gmail.com>
2026-02-20 21:54:34 -0500

    script update

--- bin/merge_new_words.sh
+++ bin/merge_new_words.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
-DIR="../.."
+DIR="."
+
+[ -f "$DIR/new_words.lst" ] || {
+  echo "No file"
+  exit 1
+}
+[ -f "data/dict/base.lst" ] || {
+  echo "No data/dict/base.lst"
+  exit 1
+}
 
 for file in geo-other; do
-  grep "#=> $file" $DIR/new_words.lst | grep -vE '^$' | sed -r "s/ *#=> $file *//" >> ${file}.lst && \
+  grep "#=> $file" $DIR/new_words.lst | grep -vE '^$' | sed -r "s/ *#=> $file *//" >> data/dict/${file}.lst
 done
-grep -v "#=> " $DIR/new_words.lst | grep -vE '^$' >> base.lst && \
+
+grep -v "#=> " $DIR/new_words.lst | grep -vE '^$' >> data/dict/base.lst && \
 mv $DIR/new_words.lst $DIR/new_words.lst.old
